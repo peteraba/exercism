08cbf6893c9795f3aad18599bfd0de7dd962516f
Peter Aba <peter@peteraba.com>
2015-04-06 09:20:15 +0200

    PHP - Wordy

---
+++ php/wordy/wordy.php
@@ -0,0 +1,38 @@
+<?php
+
+function calculate($question) {
+    //if (!preg_match_all('/^What is ([\-\d]+)(( (plus|minus|multiplied by|divided by) ([\-\d]+))+)\?$/', $question, $matches)) {
+    if (!preg_match('/^What is ([\-\d]+)(.+)\?$/', $question, $matches)) {
+        throw new InvalidArgumentException('Irrelevant question.');
+    }
+
+    $number = $matches[1];
+
+    $wordy = $matches[2];
+    while (preg_match('/^ (\D+) ([\-\d]+)/', $wordy, $matches)) {
+        switch ($matches[1]) {
+            case 'plus':
+                $number += $matches[2];
+                break;
+            case 'minus':
+                $number -= $matches[2];
+                break;
+            case 'multiplied by':
+                $number *= $matches[2];
+                break;
+            case 'divided by':
+                $number /= $matches[2];
+                break;
+            default:
+                throw new InvalidArgumentException('Too advanced.');
+        }
+
+        $wordy = mb_substr($wordy, mb_strlen($matches[0]));
+    }
+
+    if ($wordy !== '') {
+        throw new InvalidArgumentException('Too advanced.');
+    }
+
+    return $number;
+}
---
+++ php/wordy/wordy_test.php
@@ -0,0 +1,90 @@
+<?php
+
+require "wordy.php";
+
+class WordProblemTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAdd1()
+    {
+        $this->assertEquals(2, calculate('What is 1 plus 1?'));
+    }
+
+    public function testAdd2()
+    {
+        $this->assertEquals(55, calculate('What is 53 plus 2?'));
+    }
+
+    public function testAddNegativeNumbers()
+    {
+        $this->assertEquals(-11, calculate('What is -1 plus -10?'));
+    }
+
+    public function testAddMoreDigits()
+    {
+        $this->assertEquals(45801, calculate('What is 123 plus 45678?'));
+    }
+
+    public function testSubtract()
+    {
+        $this->assertEquals(16, calculate('What is 4 minus -12?'));
+    }
+
+    public function testMultiply()
+    {
+        $this->assertEquals(-75, calculate('What is -3 multiplied by 25?'));
+    }
+
+    public function testDivide()
+    {
+        $this->assertEquals(-11, calculate('What is 33 divided by -3?'));
+    }
+
+    public function testAddTwice()
+    {
+        $this->assertEquals(3, calculate('What is 1 plus 1 plus 1?'));
+    }
+
+    public function testAddThenSubtract()
+    {
+        $this->assertEquals(8, calculate('What is 1 plus 5 minus -2?'));
+    }
+
+    public function testSubtractTwice()
+    {
+        $this->assertEquals(3, calculate('What is 20 minus 4 minus 13?'));
+    }
+
+    public function testSubtractThenAdd()
+    {
+        $this->assertEquals(14, calculate('What is 17 minus 6 plus 3?'));
+    }
+
+    public function testMultiplyTwice()
+    {
+        $this->assertEquals(-12, calculate('What is 2 multiplied by -2 multiplied by 3?'));
+    }
+
+    public function testAddThenMultiply()
+    {
+        $this->assertEquals(-8, calculate('What is -3 plus 7 multiplied by -2?'));
+    }
+
+    public function testDivideTwice()
+    {
+        $this->assertEquals(2, calculate('What is -12 divided by 2 divided by -3?'));
+    }
+
+    public function testTooAdvanced()
+    {
+        $this->setExpectedException('InvalidArgumentException');
+
+        calculate('What is 53 cubed?');
+    }
+
+    public function testIrrelevant()
+    {
+        $this->setExpectedException('InvalidArgumentException');
+
+        calculate('Who is the president of the United States?');
+    }
+}
