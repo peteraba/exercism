fd788b36c8475397545c8165a71b22bf5d2d4fa3
Peter Aba <peter@peteraba.com>
2015-04-06 15:01:35 +0200

    Go - Leap

---
+++ go/leap/leap.go
@@ -0,0 +1,17 @@
+package leap
+
+func IsLeapYear(year int) bool {
+	if (year % 400 == 0) {
+		return true;
+	}
+	if (year % 100 == 0) {
+		return false;
+	}
+	if (year % 4 == 0) {
+		return true;
+	}
+
+	return false;
+}
+
+var TestVersion int = 1;
