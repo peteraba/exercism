d5ebf2609542a0778c896bb15641f370b666b309
Peter Aba <peter@peteraba.com>
2015-04-11 12:17:51 +0200

    Python - Rna transcription enhancements

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,17 +1,11 @@
+from collections import defaultdict
+
 def to_rna(dna):
-    translate = {
+    translate = defaultdict(None, {
         'C': 'G',
         'G': 'C',
         'T': 'A',
         'A': 'U',
-    }
+    })
 
-    return strtr(dna, translate)
-
-# Found on stackoverflow, recursively replaces occurances of keys with values in to_translate
-def strtr(to_translate, replace):
-    if replace and to_translate:
-        s, r = replace.popitem()
-        return r.join(strtr(subs, dict(replace)) for subs in to_translate.split(s))
-    
-    return to_translate
+    return "".join(translate[letter] for letter in dna)
