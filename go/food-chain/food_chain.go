f887a93114f41eaba73e72df40896db716637b32
Peter Aba <peter@peteraba.com>
2015-04-12 16:23:47 +0200

    Go - Food chain enhancements

--- go/food-chain/food_chain.go
+++ go/food-chain/food_chain.go
@@ -1,81 +1,89 @@
 package foodchain
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 const TestVersion = 1
 
-var ref2 = []string{``,
+const firstLine = "I know an old lady who swallowed a %s."
 
-	`I know an old lady who swallowed a fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
+const middleLine = "She swallowed the %s to catch the %s%s."
 
-	`I know an old lady who swallowed a spider.
-It wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a bird.
-How absurd to swallow a bird!
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a cat.
-Imagine that, to swallow a cat!
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a dog.
-What a hog, to swallow a dog!
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a goat.
-Just opened her throat and swallowed a goat!
-She swallowed the goat to catch the dog.
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a cow.
-I don't know how she swallowed a cow!
-She swallowed the cow to catch the goat.
-She swallowed the goat to catch the dog.
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a horse.
-She's dead, of course!`,
+type animal struct {
+	name, secondLine, thatPart string
 }
 
-func Verse(verse int) string {
-	return ref2[verse]
+var animals = []animal{
+	animal{"fly", "I don't know why she swallowed the fly. Perhaps she'll die.", ""},
+	animal{"spider", "It wriggled and jiggled and tickled inside her.", ""},
+	animal{"bird", "How absurd to swallow a bird!", " that wriggled and jiggled and tickled inside her"},
+	animal{"cat", "Imagine that, to swallow a cat!", ""},
+	animal{"dog", "What a hog, to swallow a dog!", ""},
+	animal{"goat", "Just opened her throat and swallowed a goat!", ""},
+	animal{"cow", "I don't know how she swallowed a cow!", ""},
+	animal{"horse", "She's dead, of course!", ""},
 }
 
-func Verses(verses ...int) string {
-	selected := []string{}
-
-	for verse := range verses {
-		selected = append(selected, ref2[verse+1])
+func swallowLines(n int) string {
+	if n > 7 {
+		return ""
 	}
 
-	return joinVerses(selected)
+	verse := []string{}
+
+	var current, next animal
+	var line string
+
+	for i := n; i > 1; i-- {
+		current = animals[i-1]
+		next = animals[i-2]
+		line = fmt.Sprintf(middleLine, current.name, next.name, current.thatPart)
+		verse = append(verse, line)
+	}
+
+	if n > 1 {
+		verse = append(verse, animals[0].secondLine)
+	}
+
+	return strings.Join(verse, "\n")
+}
+
+func Verse(n int) string {
+	if n < 1 || 8 < n {
+		return ""
+	}
+
+	var currentAnimal = animals[n-1]
+
+	verse := []string{
+		fmt.Sprintf(firstLine, currentAnimal.name),
+		currentAnimal.secondLine,
+	}
+
+	var extraLines string = swallowLines(n)
+
+	if extraLines != "" {
+		verse = append(verse, extraLines)
+	}
+
+	return strings.Join(verse, "\n")
+}
+
+func Verses(start, end int) string {
+	if start <= 0 || 8 < end || end < start {
+		return ""
+	}
+
+	verses := make([]string, 0, end-start+1)
+	for i := start; i <= end; i++ {
+		verses = append(verses, Verse(i))
+	}
+
+	return strings.Join(verses, "\n\n")
 }
 
 func Song() string {
-	return joinVerses(ref2)
-}
-
-func joinVerses(verses []string) string {
-	return strings.Trim(strings.Join(verses, "\n\n"), " \n\t")
+	return Verses(1, 8)
 }
