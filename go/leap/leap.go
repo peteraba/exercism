f63183a7d0f204c0cf21fd78964502c4dad42350
Peter Aba <peter@peteraba.com>
2015-04-12 22:39:24 +0200

    Go - Leap enhancement

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,17 +1,17 @@
 package leap
 
 func IsLeapYear(year int) bool {
-	if (year % 400 == 0) {
-		return true;
+	if year%400 == 0 {
+		return true
 	}
-	if (year % 100 == 0) {
-		return false;
+	if year%100 == 0 {
+		return false
 	}
-	if (year % 4 == 0) {
-		return true;
+	if year%4 == 0 {
+		return true
 	}
 
-	return false;
+	return false
 }
 
-var TestVersion int = 1;
+var TestVersion int = 1
