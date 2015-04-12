413a724c532ccb6c9820391c22b731546f2b9f0d
Peter Aba <peter@peteraba.com>
2015-04-12 12:34:44 +0200

    Go - Triangle

---
+++ go/triangle/triangle.go
@@ -0,0 +1,47 @@
+package triangle
+
+import "math"
+
+type Kind int
+
+const (
+	Equ Kind = iota
+	Iso Kind = iota
+	Sca Kind = iota
+	NaT Kind = iota
+)
+
+func KindFromSides(a float64, b float64, c float64) Kind {
+	// Check Infinite sides
+	if math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) {
+		return NaT
+	}
+
+	// Check Not-a-number sides
+	if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) {
+		return NaT
+	}
+
+	// Check negative sides
+	if a <= 0.0 || b <= 0.0 || c <= 0.0 {
+		return NaT
+	}
+
+	// Check invalid triangles
+	if a+b <= c || a+c <= b || b+c <= a {
+		return NaT
+	}
+
+	// Equ: All sides are equal
+	if a == b && b == c {
+		return Equ
+	}
+
+	// Iso: Two sides are equal but not all three
+	if a == b || b == c || a == c {
+		return Iso
+	}
+
+	// Sca: Valid triangles without equal sides
+	return Sca
+}
