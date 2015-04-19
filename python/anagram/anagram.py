d7886f12e78658c3c6964602be9ed9d8161639d8
Peter Aba <peter@peteraba.com>
2015-04-19 20:05:54 +0200

    Python - Anagram enhancements

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,16 +1,12 @@
 def detect_anagrams(search, haystack):
     found = []
-    
 
     search = search.lower()
-    normalised_search = normalise_string(search)
+    normalised_search = sorted(search)
 
     for word in haystack:
-        if word.lower() != search and normalised_search == normalise_string(word.lower()):
+        if word.lower() != search and normalised_search == sorted(word.lower()):
             found.append(word)
 
     return found
-    
-def normalise_string(orig_string):
-    return "".join(sorted(orig_string))
 
