ec3cf6b83afbe724ca68cf47afa984e39e4b6db2
Peter Aba <peter@peteraba.com>
2018-01-03 23:02:06 +0100

    F# Leap and Bob

---
+++ fsharp/bob/Bob.fs
@@ -0,0 +1,14 @@
+﻿module Bob
+
+open System.Text.RegularExpressions
+
+let response (input: string): string =
+    let trimmed = input.Trim()
+    let isYelled = trimmed = trimmed.ToUpper() && not (trimmed = trimmed.ToLower())
+    let isQuestion = trimmed.Length > 0 && trimmed.[trimmed.Length - 1] = '?'
+
+    if trimmed.Length = 0 then "Fine. Be that way!"
+    elif isYelled && isQuestion then "Calm down, I know what I'm doing!"
+    elif isYelled then "Whoa, chill out!"
+    elif isQuestion then "Sure."
+    else "Whatever."
---
+++ fsharp/leap/Leap.fs
@@ -0,0 +1,9 @@
+﻿module Leap
+
+let leapYear (year: int): bool = 
+    if year % 4 = 0 then
+        if year % 100 = 0 then
+            if year % 400 = 0 then true
+            else false
+        else true
+    else false
