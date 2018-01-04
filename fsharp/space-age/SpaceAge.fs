093d5e7ae8708af2a1da6b1aec68dfb531dc205c
Peter Aba <peter@peteraba.com>
2018-01-04 04:18:28 +0100

    F# space age

---
+++ fsharp/space-age/SpaceAge.fs
@@ -0,0 +1,20 @@
+﻿module SpaceAge
+
+let private year = 31557600m
+let private workaround = 1000000000m
+
+// TODO: define the Planet type
+type Planet =
+    | Earth    = 1000000000UL
+    | Mercury  = 240846700UL
+    | Venus    = 615197260UL
+    | Mars     = 1880815800UL
+    | Jupiter  = 11862615000UL
+    | Saturn   = 29447498000UL
+    | Uranus   = 84016846000UL
+    | Neptune  = 164791320000UL
+
+let spaceAge (planet: Planet) (seconds: decimal): decimal =
+    let p = uint64 planet
+    let planet_year_in_seconds = decimal p / workaround * year
+    System.Math.Round(seconds / planet_year_in_seconds, 2)
---
+++ fsharp/space-age/SpaceAgeTest.fs
@@ -0,0 +1,52 @@
+module SpaceAgeTest
+
+open Xunit
+open FsUnit.Xunit
+open SpaceAge
+    
+[<Fact>]
+let ``Age on earth`` () =
+    let seconds = 1000000000m
+    spaceAge Planet.Earth seconds |> should equal 31.69m
+
+[<Fact>]
+let ``Age on mercury`` () =
+    let seconds = 2134835688m
+    spaceAge Planet.Earth seconds |> should equal 67.65m
+    spaceAge Planet.Mercury seconds |> should equal 280.88m
+
+[<Fact>]
+let ``Age on venus`` () =
+    let seconds = 189839836m
+    spaceAge Planet.Earth seconds |> should equal 6.02m
+    spaceAge Planet.Venus seconds |> should equal 9.78m
+
+[<Fact>]
+let ``Age on mars`` () =
+    let seconds = 2329871239m
+    spaceAge Planet.Earth seconds |> should equal 73.83m
+    spaceAge Planet.Mars seconds |> should equal 39.25m
+
+[<Fact>]
+let ``Age on jupiter`` () =
+    let seconds = 901876382m
+    spaceAge Planet.Earth seconds |> should equal 28.58m
+    spaceAge Planet.Jupiter seconds |> should equal 2.41m
+
+[<Fact>]
+let ``Age on saturn`` () =
+    let seconds = 3000000000m
+    spaceAge Planet.Earth seconds |> should equal 95.06m
+    spaceAge Planet.Saturn seconds |> should equal 3.23m
+
+[<Fact>]
+let ``Age on uranus`` () =
+    let seconds = 3210123456m
+    spaceAge Planet.Earth seconds |> should equal 101.72m
+    spaceAge Planet.Uranus seconds |> should equal 1.21m
+
+[<Fact>]
+let ``Age on neptune`` () =
+    let seconds = 8210123456m
+    spaceAge Planet.Earth seconds |> should equal 260.16m
+    spaceAge Planet.Neptune seconds |> should equal 1.58m
