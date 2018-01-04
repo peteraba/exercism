ed4ece230ed3b3654c29e10f2b15eaf7c50165b2
Peter Aba <peter@peteraba.com>
2018-01-04 23:30:48 +0100

    F# add raindrops

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,11 @@
+﻿module Raindrops
+
+let private drops = [ ( 3, "Pling" ); ( 5, "Plang" ); ( 7, "Plong" ) ]
+
+let convert (number: int): string =
+    let mutable say = ""
+    for drop in drops do
+        let (divider, sound) = drop
+        say <- if number % divider = 0 then say + sound; else say;
+    
+    if say = "" then string number else say
