c7790187793a7f7be2b55e632234beed2cffd7d0
Peter Aba <peter@peteraba.com>
2018-01-05 02:17:54 +0100

    F# Add ArmstrongNumbers

---
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,22 @@
+﻿module ArmstrongNumbers
+
+let private pow (number: int) (power: int): int =
+    let mutable s = number
+
+    for i = 1 to power - 1 do
+        s <- s * number
+
+    s
+    
+let isArmstrongNumber (number: int): bool =
+    let str = string number
+    let len = str.Length
+    let mutable powered = 0
+
+    for i = 0 to len-1 do
+        let value = str.[i..i] |> int
+        let p = pow value len
+        powered <- powered + p
+
+    number = powered
+
