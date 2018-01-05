17eb45a79c13a31f6b46b0a0c4c9a2517873d901
Peter Aba <peter@peteraba.com>
2018-01-05 01:14:07 +0100

    F# add collatz conjecture

---
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -0,0 +1,20 @@
+﻿module CollatzConjecture
+
+let rec private substeps (number: int) (count: int): int =
+    match number with
+    | x when x % 2 = 0 -> substeps (number / 2) (count + 1)
+    | x when x > 1 -> substeps (number * 3 + 1) (count + 1)
+    | _ -> count
+
+let steps (number: int): int option =
+    let mutable count = 0
+    let mutable y = number
+
+    match number with
+    | x when x <= 0 -> None
+    | _ ->
+        while y > 1 do
+            y <- if y % 2 = 0 then y / 2; else 3 * y + 1;
+            count <- count + 1
+
+        Some(count)
