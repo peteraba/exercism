1927474a206e7279fc0bbf2f4c708ccbf196bc15
Peter Aba <peter.aba@zalando-lounge.de>
2018-01-06 15:24:16 +0100

    F# Ripping of JJacobX's perfect solution for BearSong

--- fsharp/beer-song/BeerSong.fs
+++ fsharp/beer-song/BeerSong.fs
@@ -1,41 +1,38 @@
 ﻿module BeerSong
 
-let private bottles (currentBottles: int): string =
-    let bottlesCount =
-        match currentBottles with
-        | x when x > 1 -> string x + " bottles of beer"
-        | x when x = 1 -> string x + " bottle of beer"
-        | _ -> "no more bottles of beer"
+let recite (startBottles: int) (takeDown: int) = 
 
-    bottlesCount
-    
-let private firstLine (currentBottles: int): string =
-    let bottlesCount = bottles currentBottles
+    let printVerse = 
+        function
+        | x when x > 2 ->
+            [ 
+                sprintf "%d bottles of beer on the wall, %d bottles of beer." x x;
+                sprintf "Take one down and pass it around, %d bottles of beer on the wall." (x - 1);
+                "" 
+            ]
+        | x when x = 2 ->
+            [
+                "2 bottles of beer on the wall, 2 bottles of beer.";
+                "Take one down and pass it around, 1 bottle of beer on the wall.";
+                ""
+            ]
+        | x when x = 1 ->
+            [
+                "1 bottle of beer on the wall, 1 bottle of beer.";
+                "Take it down and pass it around, no more bottles of beer on the wall.";
+                ""
+            ]
+        | _ ->
+            [
+                "No more bottles of beer on the wall, no more bottles of beer.";
+                "Go to the store and buy some more, 99 bottles of beer on the wall."
+            ]
 
-    let capitalizedBottlesCount = bottlesCount.Substring(0, 1).ToUpper() + bottlesCount.Substring(1)
-
-    capitalizedBottlesCount + " on the wall, " + bottlesCount + "."
-    
-let private secondLine (currentBottles: int): string =
-    let action =
-        match currentBottles with
-        | x when x > 1 -> "Take one down and pass it around"
-        | x when x = 1 -> "Take it down and pass it around"
-        | _ -> "Go to the store and buy some more"
-    
-    let newBottlesCount =
-        match currentBottles with
-        | x when x = 0 -> bottles 99
-        | _ -> bottles (currentBottles - 1)
-    
-    action + ", " + newBottlesCount + " on the wall."
-
-let recite (startBottles: int) (takeDown: int) =
-    let mutable verses = []
-    let mutable bottles = startBottles
-
-    for i = 1 to takeDown do
-        verses <- verses @ [ firstLine bottles; secondLine bottles; "" ]
-        bottles <- if bottles = 0 then 99; else bottles - 1;
-
-    if verses.Length > 2 then verses.[0 .. verses.Length-2]; else []
+    let generate = 
+        function
+        | x when x >= 0 -> Some (printVerse x, x - 1)
+        | _             -> None
+        
+    List.unfold generate startBottles
+    |> List.concat
+    |> List.take (takeDown * 3 - 1)
