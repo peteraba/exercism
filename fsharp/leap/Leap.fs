d58cef6d95e647d7686e421bb729176f9b8e2d57
Peter Aba <peter.aba@zalando-lounge.de>
2018-01-06 13:39:36 +0100

    F# Leap self-improved

--- fsharp/leap/Leap.fs
+++ fsharp/leap/Leap.fs
@@ -1,9 +1,7 @@
 ﻿module Leap
 
 let leapYear (year: int): bool = 
-    if year % 4 = 0 then
-        if year % 100 = 0 then
-            if year % 400 = 0 then true
-            else false
-        else true
-    else false
+    match year with
+    | x when x % 4 <> 0 -> false
+    | x when x % 100 = 0 -> x % 400 = 0
+    | _ -> true
