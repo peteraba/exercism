4ed6027589542101408c2ef24362ac00ea3f7373
Peter Aba <peter.aba@zalando-lounge.de>
2018-01-06 22:10:57 +0100

    F# Add BinarySearchTree

---
+++ fsharp/binary-search-tree/BinarySearchTree.fs
@@ -0,0 +1,52 @@
+﻿module BinarySearchTree
+
+type Tree = {
+    Value: int option;
+    Left: Tree option;
+    Right: Tree option;
+}
+
+let left node  =
+    node.Left
+
+let right node =
+    node.Right
+
+let value node =
+    node.Value
+
+let singleton value =
+    { Value = Some(value); Left = None; Right = None }
+
+let rec insert newValue tree =
+    match tree.Value with
+    | None -> { tree with Value = Some(newValue) }
+    | Some(x) when x >= newValue ->
+        match tree.Left with
+        | None -> { tree with Left = Some(singleton newValue) }
+        | Some(y) -> { tree with Left = Some(insert newValue y) }
+    | Some(x) ->
+        match tree.Right with
+        | None -> { tree with Right = Some(singleton newValue) }
+        | Some(y) -> { tree with Right = Some(insert newValue y) }
+
+let toList tree =
+    let rec accToList trees ints =
+        match trees with 
+        | [] -> ints
+        | { Value = None; Left = _; Right = _ } :: tail -> failwith "Invalid node found"
+        | { Value = Some(v); Left = l; Right = r } :: tail ->
+            let newTrees =
+                match l, r with
+                | None, None -> tail
+                | Some(x), None -> x :: tail
+                | None, Some(y) -> y :: tail
+                | Some(x), Some(y) -> x :: y :: tail
+
+            accToList newTrees (v :: ints)
+
+    List.sort (accToList [tree] [])
+
+let fromList xs =
+    let e = { Value = None; Left = None; Right = None }
+    List.fold (fun acc elem -> insert elem acc) e xs
---
+++ fsharp/binary-search-tree/BinarySearchTreeTest.fs
@@ -0,0 +1,59 @@
+module BinarySearchTreeTest
+
+open Xunit
+open FsUnit.Xunit
+open BinarySearchTree
+
+[<Fact>]
+let ``Data is retained`` () =
+    let tree = singleton 4
+    tree |> value |> should equal <| Some 4
+
+[<Fact>]
+let ``Inserting less`` () =
+    let tree = singleton 4 |> insert 2
+    tree |> value |> should equal <| Some 4
+    tree |> left |> Option.bind value |> should equal <| Some 2
+
+[<Fact>]
+let ``Inserting same`` () =
+    let tree = singleton 4 |> insert 4
+    tree |> value |> should equal <| Some 4
+    tree |> left |> Option.bind value |> should equal <| Some 4
+
+[<Fact>]
+let ``Inserting greater`` () =
+    let tree = singleton 4 |> insert 5
+    tree |> value |> should equal <| Some 4
+    tree |> right |> Option.bind value |> should equal <| Some 5
+
+[<Fact>]
+let ``Complex tree`` () =
+    let tree = fromList [4; 2; 6; 1; 3; 7; 5]
+    tree |> value |> should equal <| Some 4
+    tree |> left |> Option.bind value |> should equal <| Some 2
+    tree |> left |> Option.bind (fun x -> x |> left) |> Option.bind value |> should equal <| Some 1
+    tree |> left |> Option.bind (fun x -> x |> right) |> Option.bind value |> should equal <| Some 3
+    tree |> right |> Option.bind value |> should equal <| Some 6
+    tree |> right |> Option.bind (fun x -> x |> left) |> Option.bind value |> should equal <| Some 5
+    tree |> right |> Option.bind (fun x -> x |> right) |> Option.bind value |> should equal <| Some 7
+
+[<Fact>]
+let ``Iterating one element`` () =
+    let elements = singleton 4 |> toList
+    elements |> should equal [4]
+
+[<Fact>]
+let ``Iterating over smaller element`` () =
+    let elements = fromList [4; 2] |> toList
+    elements |> should equal [2; 4]
+
+[<Fact>]
+let ``Iterating over larger element`` () =
+    let elements = fromList [4; 5] |> toList
+    elements |> should equal [4; 5]
+
+[<Fact>]
+let ``Iterating over complex element`` () =
+    let elements = fromList [4; 2; 1; 3; 6; 7; 5] |> toList
+    elements |> should equal [1; 2; 3; 4; 5; 6; 7] 
