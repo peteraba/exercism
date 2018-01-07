4537fa23efd44b846d3769b7ac88cb93e06797a6
Peter Aba <peter@peteraba.com>
2018-01-07 16:21:10 +0100

    F# Grains

---
+++ fsharp/grains/Grains.fs
@@ -0,0 +1,21 @@
+﻿module Grains
+
+type Result<'a, 'b> =
+    | Ok of 'a
+    | Error of 'b
+
+let square (n: int): Result<uint64,string> =
+    match n with
+    | x when x < 1 || x > 64 -> Error("Invalid input")
+    | x -> Ok(1UL <<< (x - 1))
+
+let total: Result<uint64,string> =
+    // Hardcoded would be hard to outperform: Ok(18446744073709551615UL)
+
+    let resultSum x =
+        match x with
+        | Ok(n) -> n
+        | Error(e) -> 0UL
+
+    [ for i in 1 .. 64 -> square i ] |> List.sumBy resultSum |> Ok
+
