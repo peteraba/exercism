f73baaf95d061d84424fccc82a9b3ed87667eb4d
Peter Aba <peter.aba@zalando-lounge.de>
2018-01-07 01:15:50 +0100

    F# Add DifferenceOfSquares

---
+++ fsharp/difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,10 @@
+﻿module DifferenceOfSquares
+
+let squareOfSum (number: int): int =
+    List.sum [1 .. number] |> (fun x -> x * x)
+
+let sumOfSquares (number: int): int =
+    List.sumBy (fun x -> x * x) [1 .. number]
+
+let differenceOfSquares (number: int): int =
+    squareOfSum number - sumOfSquares number
