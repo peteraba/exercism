c405f6a7067bcb1e9b6d8c2c67e71f06df2cafb5
Peter Aba <peter@peteraba.com>
2018-01-07 17:09:29 +0100

    F# Hamming reformatted

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -3,6 +3,10 @@
 let distance (strand1: string) (strand2: string): int option =
     match strand1.Length, strand2.Length with
     | x, y when x = y ->
-        let d = List.fold2 (fun acc a b -> if a=b then acc; else acc + 1) 0 (strand1 |> List.ofSeq) (strand2 |> List.ofSeq) 
+        let folder acc a b =
+            if a = b then acc; else acc + 1
+
+        let d = List.fold2 folder 0 (List.ofSeq strand1) (List.ofSeq strand2)
+        
         Some(d)
     | _, _ -> None
