ead47aeea49668b58bd9f68fb0ac31ffd3655508
Peter Aba <peter@peteraba.com>
2018-01-07 16:54:04 +0100

    F# Hamming

---
+++ fsharp/hamming/Hamming.fs
@@ -0,0 +1,8 @@
+﻿module Hamming
+
+let distance (strand1: string) (strand2: string): int option =
+    match strand1.Length, strand2.Length with
+    | x, y when x = y ->
+        let d = List.fold2 (fun acc a b -> if a=b then acc; else acc + 1) 0 (strand1 |> List.ofSeq) (strand2 |> List.ofSeq) 
+        Some(d)
+    | _, _ -> None
