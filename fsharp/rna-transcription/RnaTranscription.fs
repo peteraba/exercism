12520d9e1a0bc6a4865a7a058cc904f035345910
Peter Aba <peter@peteraba.com>
2018-01-07 14:45:42 +0100

    F# Add RnaTranscription

---
+++ fsharp/rna-transcription/RnaTranscription.fs
@@ -0,0 +1,17 @@
+﻿module RnaTranscription
+
+let toRna (dna: string): string option =
+    let translateChar ch =
+        match ch with
+        | 'G' -> 'C'
+        | 'C' -> 'G'
+        | 'T' -> 'A'
+        | 'A' -> 'U'
+        | _ -> failwith "Invalid RNA"
+    
+    try
+        let rna = Seq.map translateChar dna |> System.String.Concat
+       
+        Some(rna)
+    with
+        | Failure(msg) -> None
