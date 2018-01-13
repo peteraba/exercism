e938358a72fb1870fac7ffa12b696d6f6b3a49f8
Peter Aba <peter@peteraba.com>
2018-01-13 14:02:56 +0100

    F# Sieve

---
+++ fsharp/sieve/Sieve.fs
@@ -0,0 +1,12 @@
+﻿module Sieve
+
+let primesUpTo limit =
+    let sq = (limit |> float |> (fun n -> n ** 0.5) |> int )
+    
+    let filterMultiples possiblePrimes prime =
+        if prime > sq then possiblePrimes
+        else possiblePrimes |> List.filter (fun x -> x <= prime || x % prime <> 0)
+
+    let possiblePrimes = [ 2 .. limit ]
+
+    List.fold filterMultiples possiblePrimes possiblePrimes
