b5583ff69d48317fb046f2fd899511fe31982b5e
Peter Aba <peter@peteraba.com>
2018-01-18 22:39:59 +0100

    F# Anagram

---
+++ fsharp/anagram/Anagram.fs
@@ -0,0 +1,15 @@
+﻿module Anagram
+
+open System
+
+let anagrams sources (target: string) =
+    let lowerTarget = target.ToLower()
+    let sortedTarget = lowerTarget |> Seq.sort |> String.Concat
+    
+    let isSame (source: string): bool =
+        let lowerSource = source.ToLower()
+
+        if lowerSource = lowerTarget then false
+        else sortedTarget = (lowerSource |> Seq.sort |> String.Concat)
+
+    sources |> List.filter isSame
