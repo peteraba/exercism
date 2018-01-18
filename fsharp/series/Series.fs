e3834ad474136e1d012b3877c69e7c15b20c0370
Peter Aba <peter@peteraba.com>
2018-01-18 23:08:28 +0100

    F# Series

---
+++ fsharp/series/Series.fs
@@ -0,0 +1,11 @@
+﻿module Series
+open System
+
+let slices (str: string) length =
+    if length > str.Length then raise (ArgumentException("Invalid length"))
+    else
+        let sliceLength = str.Length - length
+        let numToDigitList (num: string) =
+            num |> Seq.map (string >> System.Int32.Parse) |> List.ofSeq
+
+        [ for i in 0 .. sliceLength -> str.[i .. i + length - 1] |> numToDigitList ]
---
+++ fsharp/series/SeriesTest.fs
@@ -0,0 +1,70 @@
+module SeriesTest
+
+open System
+open Xunit
+open FsUnit.Xunit
+open Series
+
+type SeriesTests () =
+    static member SliceOneTestData = 
+        let theoryData = new TheoryData<string, int list list>()
+        theoryData.Add("01234", [[0]; [1]; [2]; [3]; [4]])
+        theoryData.Add("92834", [[9]; [2]; [8]; [3]; [4]])
+        theoryData
+
+    [<Theory>]
+    [<MemberData("SliceOneTestData")>]
+    member this.``Series of one splits to one digit`` input expected =
+        slices input 1 |> should equal expected
+
+    static member SliceTwoTestData = 
+        let theoryData = new TheoryData<string, int list list>()
+        theoryData.Add("01234", [[0; 1]; [1; 2]; [2; 3]; [3; 4]]);
+        theoryData.Add("98273463", [[9; 8]; [8; 2]; [2; 7]; [7; 3]; [3; 4]; [4; 6]; [6; 3]]);
+        theoryData.Add("37103", [[3; 7]; [7; 1]; [1; 0]; [0; 3]])
+        theoryData
+
+    [<Theory>]
+    [<MemberData("SliceTwoTestData")>]
+    member this.``Series of two splits to two digits`` input expected =
+        slices input 2 |> should equal expected
+
+    static member SliceThreeTestData = 
+        let theoryData = new TheoryData<string, int list list>()
+        theoryData.Add("01234", [[0; 1; 2]; [1; 2; 3]; [2; 3; 4]]);
+        theoryData.Add("31001", [[3; 1; 0]; [1; 0; 0]; [0; 0; 1]]);
+        theoryData.Add("982347", [[9; 8; 2]; [8; 2; 3]; [2; 3; 4]; [3; 4; 7]])
+        theoryData
+
+    [<Theory>]
+    [<MemberData("SliceThreeTestData")>]
+    member this.``Series of three splits to three digits`` input expected =
+        slices input 3 |> should equal expected
+
+    static member SliceFourTestData = 
+        let theoryData = new TheoryData<string, int list list>()
+        theoryData.Add("01234", [[0; 1; 2; 3]; [1; 2; 3; 4]]);
+        theoryData.Add("91274", [[9; 1; 2; 7]; [1; 2; 7; 4]])
+        theoryData
+
+    [<Theory>]
+    [<MemberData("SliceFourTestData")>]
+    member this.``Series of four splits to four digits`` input expected =
+        slices input 4 |> should equal expected
+
+    static member SliceFiveTestData = 
+        let theoryData = new TheoryData<string, int list list>()
+        theoryData.Add("01234", [[0; 1; 2; 3; 4]]);
+        theoryData.Add("81228", [[8; 1; 2; 2; 8]])
+        theoryData
+
+    [<Theory>]
+    [<MemberData("SliceFiveTestData")>]
+    member this.``Series of five splits to five digits`` input expected =
+        slices input 5 |> should equal expected
+
+    [<Theory>]
+    [<InlineData("01234", 6)>]
+    [<InlineData("01032987583", 19)>]
+    member this.``Slice longer than input is not allowed`` input slice =
+        (fun () -> slices input slice |> ignore) |> should throw typeof<ArgumentException>
