7941b2bb0ddc7e8ad7c51757208ddd5b58ba145c
Peter Aba <peter@peteraba.com>
2018-02-03 13:31:54 +0100

    F# Pascals Triangle

---
+++ fsharp/pascals-triangle/PascalsTriangle.fs
@@ -0,0 +1,20 @@
+﻿module PascalsTriangle
+
+let rows numberOfRows : int list list option =
+    let row (previousRow: int list) : int list =
+        [
+            for i in 0 .. previousRow.Length ->
+                if i = 0 then 1
+                elif i = previousRow.Length then 1
+                else previousRow.[i - 1] + previousRow.[i]
+        ]
+
+    let unfolder (currentRow, previous) =
+        if currentRow >= numberOfRows then None 
+        else 
+            let thisRow = row previous
+            Some(thisRow, (currentRow + 1, thisRow))
+
+
+    if numberOfRows < 0 then None
+    else Seq.unfold unfolder (0, []) |> List.ofSeq |> Some
