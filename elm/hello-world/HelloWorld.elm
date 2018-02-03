68534b224a2341fe72eb656d22e6ceef5b0ae03f
Peter Aba <peter@peteraba.com>
2018-02-03 20:51:38 +0100

    Elm small fix

--- elm/hello-world/HelloWorld.elm
+++ elm/hello-world/HelloWorld.elm
@@ -5,11 +5,3 @@ helloWorld name =
     case name of
         Just x -> "Hello, " ++ x ++ "!"
         Nothing -> "Hello, World!"
-
-
-
-{-
-   When you have a working solution, REMOVE ALL THE STOCK COMMENTS.
-   They're here to help you get started but they only clutter a finished solution.
-   If you leave them in, nitpickers will protest!
--}
